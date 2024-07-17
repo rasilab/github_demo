ee204f9b82df836e9ff80f4fb77153d2fb09cfb9
Katharine Chen <kychen37@uw.edu>
2024-07-17 09:27:31 -0700

    remove unneeded files

--- manuscripts/kchen/dicodon/maintext/make_manuscript.sh
+++
@@ -1,3 +0,0 @@
-# To create the container used below, run the following once per computer:
-# docker run -i -d --name test -v $HOME:$HOME ghcr.io/rasilab/project_repo
-docker exec -w /workspaces/rqc_aggregation_aging/manuscripts/dicodon/maintext naughty_kapitsa pandoc maintext.md --filter=pandoc-svg.py --citeproc --template=template.tex --metadata-file=pandoc-metadata.yaml --pdf-engine=xelatex -o maintext.pdf
